f1ce746f9befedb3e492ae45a81fe710b8a7708d
Rapsssito <contact@rodrigomartin.dev>
2020-11-04 22:54:01 +0100

    Add bot name

--- src/content.js
+++ src/content.js
@@ -11,7 +11,7 @@ function addBotToPage() {
           id="${BOT_ELEMENT_ID}"
           chat-icon="https://storage.googleapis.com/cloudprod-apiai/4af2327e-4141-4e9e-b654-d99f1be6b5e8_x.png"
           intent="WELCOME"
-          chat-title="Test"
+          chat-title="MoodleBot"
           agent-id="4a478516-0606-483a-858a-0a528eeecb96"
           language-code="es"
           style="z-index: 99"
